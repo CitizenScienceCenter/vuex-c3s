683f578f655dac490c3347ef70b70a16e1a6a284
encima <encima@gmail.com>
2018-11-09 14:20:32 +0100

    initial commit

--- dist/vuex-c3s.cjs.js
+++ dist/vuex-c3s.cjs.js
@@ -33,24 +33,21 @@ var C3SPlugin = {};
 C3SPlugin.install = function install(Vue) {
   var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
 
-  console.log(options);
   if (!options.store) console.log('Please provide a store!!');
   if (!options.swaggerURL) console.log('Please provide a Swagger Path!!');
 
-  //   options.store.registerModule('c3s', store)
+  options.store.registerModule('c3s', store);
 
   Swagger({
-    // url: options.swaggerURL,
-    // requestInterceptor (req) {
-    //   // req.headers['content-type'] = 'application/json'
-    //   let u = store.getters['user/currentUser']
-    //   if (u !== null) {
-    //     req.headers['X-API-KEY'] = u.api_key
-    //   }
-    //   return req
-    // }
+    url: options.swaggerURL,
+    requestInterceptor: function requestInterceptor(req) {
+      var u = store.getters['user/currentUser'];
+      if (u !== null) {
+        req.headers['X-API-KEY'] = u.api_key;
+      }
+      return req;
+    }
   }).then(function (client) {
-    console.log(client);
     store.commit('api/SET_API', client);
   }).catch(function (err) {
     console.error('URL was not found or an initialisation error occurred');
